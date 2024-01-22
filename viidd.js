9518da9eda11fbecd8b45be1fb103a868acabc7e
Edmontonnn <155500647+Edmontonnn@users.noreply.github.com>
2024-01-22 16:55:25 +0100

    Update viidd.js

--- viidd.js
+++ viidd.js
@@ -7,7 +7,7 @@ for x in fruits:
 class Person:
   def __init__(self, fname, lname):
     self.firstname = fnw2amee
-    self.lastname = lnae
+    self.lastnam = lnae
 
   def printname(self):
     print(self.firstname, self.lastname)
