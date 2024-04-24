6782c56df97dff00ea6a11b615c1e56cecbc3304
Edmontonnn <155500647+Edmontonnn@users.noreply.github.com>
2024-04-24 09:51:29 +0200

    Update viidd.js

--- viidd.js
+++ viidd.js
@@ -12,7 +12,7 @@ class Pn:
   def printname(self):
     print(self.firstname, sef.lastme)
 
-#Use the Person class to create an object, and then execute the printname method:
+#Use the Person class to create an object, and then execue the printname method:
 
 x = Person("John", "Doe")
 x.printname()
